1a58f748dd2a50b9c9012d940e8dae59a611b258
biedrzycki105 <biedrzycki105@gmail.com>
2023-09-20 00:22:06 -0400

    IP Assignment

--- NetworkTriage/ip.sh
+++ NetworkTriage/ip.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-ip addr | awk '$1 ~ /^inet/ && $1 !~ /inet6/ {print $2}'
+
+i=0
+
+for item in `ip a | awk '$1 ~ /^inet/ && $1 !~ /inet6/ { print $2}'`; do
+  arr[$i]=$item
+  i=`expr $i + 1`
+done
+
+i=0
+for item in ${arr[*]}; do
+  echo "interface" $i: ${arr[$i]}
+  echo ""
+  i=`expr $i + 1`
+done
+
+
